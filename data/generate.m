bd9cc0548f23e9b70bcff798b9c2a6229ac6f654
gaoshiucla <gaoshinihao@gmail.com>
2011-11-28 21:28:56 -0800

    Update for new 10 D data

--- data/generate.m
+++ data/generate.m
@@ -3,7 +3,7 @@ function [] = generate(len)
 cnt = 0;
 
 res = rand(len, 10);
-res0 = rand(len, 12);
+res0 = rand(len, 11);
 
 res = 2000000 * res - 1000000;
 
@@ -12,14 +12,13 @@ for i = 1:len
     k = sum(res(i,:));
     res0(i,1:10) = res(i,:);
     res0(i,11) = -1;
-    res0(i,12) = -1;
     if(k > 0)
         cnt = cnt + 1;
-        res0(i,12) = 1;
+        res0(i,11) = 1;
     end
 end
 
-dlmwrite('test.txt', res0, 'precision', '%.0f');
+dlmwrite('train.txt', res0, 'precision', '%.0f');
 
 disp(cnt);
 disp(len - cnt);
