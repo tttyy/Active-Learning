fe9a152d4acdc59a16ea7336917939abe04e9c75
gaoshiucla <gaoshinihao@gmail.com>
2011-11-29 20:19:05 -0800

    Update slides

--- qbc/compute.m
+++ qbc/compute.m
@@ -2,11 +2,13 @@ function [] = compute()
 
 err = zeros(100,1);
 
-for i = 1:100
-    x = (-0.67) * i * 10;
-    x = x / 100;
+for i = 1:20
+    j = i * 100;
+    x = (-0.67) * j;
+    x = x / 60000;
     err(i) = 50 * (2^x);
-    disp([i*10 err(i)]);
+    err(i) = 1 - (err(i)/100);
+    disp(['[' num2str(j) ',' num2str(err(i)) '],']);
 end
 
 %dlmwrite('theoryerror.txt', err, 'precision', '%.4f');
